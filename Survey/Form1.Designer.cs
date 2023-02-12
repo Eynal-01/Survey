09f8e99bf688548499582bef24e686ffcff0cf27
Baxsh_cw07@itstep.edu.az <Baxsh_cw07@itstep.edu.az>
2023-02-12 20:38:46 +0400

    Add project files.

---
+++ Survey/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Survey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SurveygroupBox1 = new System.Windows.Forms.GroupBox();
+            this.UserListBox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Savebtn = new System.Windows.Forms.Button();
+            this.Loadbtn = new System.Windows.Forms.Button();
+            this.Changebtn = new System.Windows.Forms.Button();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.SurveygroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SurveygroupBox1
+            // 
+            this.SurveygroupBox1.BackColor = System.Drawing.Color.DimGray;
+            this.SurveygroupBox1.Controls.Add(this.Addbtn);
+            this.SurveygroupBox1.Controls.Add(this.Changebtn);
+            this.SurveygroupBox1.Controls.Add(this.textBox4);
+            this.SurveygroupBox1.Controls.Add(this.textBox2);
+            this.SurveygroupBox1.Controls.Add(this.textBox1);
+            this.SurveygroupBox1.Location = new System.Drawing.Point(12, 24);
+            this.SurveygroupBox1.Name = "SurveygroupBox1";
+            this.SurveygroupBox1.Size = new System.Drawing.Size(375, 414);
+            this.SurveygroupBox1.TabIndex = 0;
+            this.SurveygroupBox1.TabStop = false;
+            this.SurveygroupBox1.Text = "SURVEY";
+            this.SurveygroupBox1.Enter += new System.EventHandler(this.SurveygroupBox1_Enter);
+            // 
+            // UserListBox
+            // 
+            this.UserListBox.BackColor = System.Drawing.Color.DimGray;
+            this.UserListBox.FormattingEnabled = true;
+            this.UserListBox.Location = new System.Drawing.Point(404, 24);
+            this.UserListBox.Name = "UserListBox";
+            this.UserListBox.Size = new System.Drawing.Size(255, 316);
+            this.UserListBox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 212);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.DimGray;
+            this.textBox3.Location = new System.Drawing.Point(404, 356);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(255, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(61, 132);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // Savebtn
+            // 
+            this.Savebtn.BackColor = System.Drawing.Color.DimGray;
+            this.Savebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Savebtn.Location = new System.Drawing.Point(537, 396);
+            this.Savebtn.Name = "Savebtn";
+            this.Savebtn.Size = new System.Drawing.Size(122, 42);
+            this.Savebtn.TabIndex = 3;
+            this.Savebtn.Text = "Save";
+            this.Savebtn.UseVisualStyleBackColor = false;
+            // 
+            // Loadbtn
+            // 
+            this.Loadbtn.BackColor = System.Drawing.Color.DimGray;
+            this.Loadbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Loadbtn.Location = new System.Drawing.Point(404, 396);
+            this.Loadbtn.Name = "Loadbtn";
+            this.Loadbtn.Size = new System.Drawing.Size(120, 42);
+            this.Loadbtn.TabIndex = 3;
+            this.Loadbtn.Text = "Load";
+            this.Loadbtn.UseVisualStyleBackColor = false;
+            // 
+            // Changebtn
+            // 
+            this.Changebtn.BackColor = System.Drawing.Color.Transparent;
+            this.Changebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Changebtn.Location = new System.Drawing.Point(243, 314);
+            this.Changebtn.Name = "Changebtn";
+            this.Changebtn.Size = new System.Drawing.Size(108, 36);
+            this.Changebtn.TabIndex = 3;
+            this.Changebtn.Text = "Change";
+            this.Changebtn.UseVisualStyleBackColor = false;
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.BackColor = System.Drawing.Color.Transparent;
+            this.Addbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addbtn.Location = new System.Drawing.Point(243, 356);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(108, 36);
+            this.Addbtn.TabIndex = 3;
+            this.Addbtn.Text = "Add";
+            this.Addbtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 450);
+            this.Controls.Add(this.Loadbtn);
+            this.Controls.Add(this.Savebtn);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.UserListBox);
+            this.Controls.Add(this.SurveygroupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "SURVEY";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SurveygroupBox1.ResumeLayout(false);
+            this.SurveygroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox SurveygroupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox UserListBox;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Addbtn;
+        private System.Windows.Forms.Button Changebtn;
+        private System.Windows.Forms.Button Savebtn;
+        private System.Windows.Forms.Button Loadbtn;
+    }
+}
+
---
+++ Survey/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Survey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                                                                  Color.DimGray,
+                                                                  Color.Black,
+                                                                  90F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SurveygroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
