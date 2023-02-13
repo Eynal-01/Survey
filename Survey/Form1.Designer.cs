74a67ec4f6b34957b9601c160c9fb776af67becf
Baxsh_cw07 <Baxsh_cw07@STHQ0118-07>
2023-02-13 08:58:20 +0400

    vravregre

--- Survey/Form1.Designer.cs
+++ Survey/Form1.Designer.cs
@@ -29,33 +29,209 @@
         private void InitializeComponent()
         {
             this.SurveygroupBox1 = new System.Windows.Forms.GroupBox();
+            this.emailchecklbl = new System.Windows.Forms.Label();
+            this.phonechecklbl = new System.Windows.Forms.Label();
+            this.surnamechecklbl = new System.Windows.Forms.Label();
+            this.namechecklbl = new System.Windows.Forms.Label();
+            this.BirthdatetimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Phonemaskedtxtb = new System.Windows.Forms.MaskedTextBox();
+            this.Emailtxtb = new System.Windows.Forms.TextBox();
+            this.birthlbl = new System.Windows.Forms.Label();
+            this.emaillbl = new System.Windows.Forms.Label();
+            this.Phonelbl = new System.Windows.Forms.Label();
+            this.surnamelbl = new System.Windows.Forms.Label();
+            this.namelbl = new System.Windows.Forms.Label();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.Changebtn = new System.Windows.Forms.Button();
+            this.Surnametxtb = new System.Windows.Forms.TextBox();
+            this.Nametxtb = new System.Windows.Forms.TextBox();
             this.UserListBox = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.Savebtn = new System.Windows.Forms.Button();
             this.Loadbtn = new System.Windows.Forms.Button();
-            this.Changebtn = new System.Windows.Forms.Button();
-            this.Addbtn = new System.Windows.Forms.Button();
             this.SurveygroupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // SurveygroupBox1
             // 
             this.SurveygroupBox1.BackColor = System.Drawing.Color.DimGray;
+            this.SurveygroupBox1.Controls.Add(this.emailchecklbl);
+            this.SurveygroupBox1.Controls.Add(this.phonechecklbl);
+            this.SurveygroupBox1.Controls.Add(this.surnamechecklbl);
+            this.SurveygroupBox1.Controls.Add(this.namechecklbl);
+            this.SurveygroupBox1.Controls.Add(this.BirthdatetimePicker);
+            this.SurveygroupBox1.Controls.Add(this.Phonemaskedtxtb);
+            this.SurveygroupBox1.Controls.Add(this.Emailtxtb);
+            this.SurveygroupBox1.Controls.Add(this.birthlbl);
+            this.SurveygroupBox1.Controls.Add(this.emaillbl);
+            this.SurveygroupBox1.Controls.Add(this.Phonelbl);
+            this.SurveygroupBox1.Controls.Add(this.surnamelbl);
+            this.SurveygroupBox1.Controls.Add(this.namelbl);
             this.SurveygroupBox1.Controls.Add(this.Addbtn);
             this.SurveygroupBox1.Controls.Add(this.Changebtn);
-            this.SurveygroupBox1.Controls.Add(this.textBox4);
-            this.SurveygroupBox1.Controls.Add(this.textBox2);
-            this.SurveygroupBox1.Controls.Add(this.textBox1);
+            this.SurveygroupBox1.Controls.Add(this.Surnametxtb);
+            this.SurveygroupBox1.Controls.Add(this.Nametxtb);
+            this.SurveygroupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.SurveygroupBox1.Location = new System.Drawing.Point(12, 24);
             this.SurveygroupBox1.Name = "SurveygroupBox1";
             this.SurveygroupBox1.Size = new System.Drawing.Size(375, 414);
             this.SurveygroupBox1.TabIndex = 0;
             this.SurveygroupBox1.TabStop = false;
             this.SurveygroupBox1.Text = "SURVEY";
-            this.SurveygroupBox1.Enter += new System.EventHandler(this.SurveygroupBox1_Enter);
+            // 
+            // emailchecklbl
+            // 
+            this.emailchecklbl.AutoSize = true;
+            this.emailchecklbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.emailchecklbl.Location = new System.Drawing.Point(112, 216);
+            this.emailchecklbl.Name = "emailchecklbl";
+            this.emailchecklbl.Size = new System.Drawing.Size(0, 13);
+            this.emailchecklbl.TabIndex = 8;
+            // 
+            // phonechecklbl
+            // 
+            this.phonechecklbl.AutoSize = true;
+            this.phonechecklbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.phonechecklbl.Location = new System.Drawing.Point(112, 171);
+            this.phonechecklbl.Name = "phonechecklbl";
+            this.phonechecklbl.Size = new System.Drawing.Size(0, 13);
+            this.phonechecklbl.TabIndex = 8;
+            // 
+            // surnamechecklbl
+            // 
+            this.surnamechecklbl.AutoSize = true;
+            this.surnamechecklbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.surnamechecklbl.Location = new System.Drawing.Point(112, 118);
+            this.surnamechecklbl.Name = "surnamechecklbl";
+            this.surnamechecklbl.Size = new System.Drawing.Size(0, 13);
+            this.surnamechecklbl.TabIndex = 8;
+            // 
+            // namechecklbl
+            // 
+            this.namechecklbl.AutoSize = true;
+            this.namechecklbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.namechecklbl.Location = new System.Drawing.Point(112, 70);
+            this.namechecklbl.Name = "namechecklbl";
+            this.namechecklbl.Size = new System.Drawing.Size(0, 13);
+            this.namechecklbl.TabIndex = 8;
+            // 
+            // BirthdatetimePicker
+            // 
+            this.BirthdatetimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BirthdatetimePicker.Location = new System.Drawing.Point(113, 239);
+            this.BirthdatetimePicker.Name = "BirthdatetimePicker";
+            this.BirthdatetimePicker.Size = new System.Drawing.Size(207, 20);
+            this.BirthdatetimePicker.TabIndex = 7;
+            // 
+            // Phonemaskedtxtb
+            // 
+            this.Phonemaskedtxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Phonemaskedtxtb.Location = new System.Drawing.Point(111, 142);
+            this.Phonemaskedtxtb.Mask = "(999) 00-000-00-00";
+            this.Phonemaskedtxtb.Name = "Phonemaskedtxtb";
+            this.Phonemaskedtxtb.Size = new System.Drawing.Size(208, 26);
+            this.Phonemaskedtxtb.TabIndex = 6;
+            // 
+            // Emailtxtb
+            // 
+            this.Emailtxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Emailtxtb.Location = new System.Drawing.Point(113, 188);
+            this.Emailtxtb.Name = "Emailtxtb";
+            this.Emailtxtb.Size = new System.Drawing.Size(206, 26);
+            this.Emailtxtb.TabIndex = 5;
+            this.Emailtxtb.TextChanged += new System.EventHandler(this.Emailtxtb_TextChanged);
+            // 
+            // birthlbl
+            // 
+            this.birthlbl.AutoSize = true;
+            this.birthlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.birthlbl.Location = new System.Drawing.Point(25, 239);
+            this.birthlbl.Name = "birthlbl";
+            this.birthlbl.Size = new System.Drawing.Size(88, 20);
+            this.birthlbl.TabIndex = 4;
+            this.birthlbl.Text = "Birth date";
+            // 
+            // emaillbl
+            // 
+            this.emaillbl.AutoSize = true;
+            this.emaillbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.emaillbl.Location = new System.Drawing.Point(25, 189);
+            this.emaillbl.Name = "emaillbl";
+            this.emaillbl.Size = new System.Drawing.Size(53, 20);
+            this.emaillbl.TabIndex = 4;
+            this.emaillbl.Text = "Email";
+            // 
+            // Phonelbl
+            // 
+            this.Phonelbl.AutoSize = true;
+            this.Phonelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Phonelbl.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.Phonelbl.Location = new System.Drawing.Point(25, 142);
+            this.Phonelbl.Name = "Phonelbl";
+            this.Phonelbl.Size = new System.Drawing.Size(60, 20);
+            this.Phonelbl.TabIndex = 4;
+            this.Phonelbl.Text = "Phone";
+            // 
+            // surnamelbl
+            // 
+            this.surnamelbl.AutoSize = true;
+            this.surnamelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.surnamelbl.Location = new System.Drawing.Point(25, 95);
+            this.surnamelbl.Name = "surnamelbl";
+            this.surnamelbl.Size = new System.Drawing.Size(81, 20);
+            this.surnamelbl.TabIndex = 4;
+            this.surnamelbl.Text = "Surname";
+            // 
+            // namelbl
+            // 
+            this.namelbl.AutoSize = true;
+            this.namelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.namelbl.Location = new System.Drawing.Point(25, 46);
+            this.namelbl.Name = "namelbl";
+            this.namelbl.Size = new System.Drawing.Size(55, 20);
+            this.namelbl.TabIndex = 4;
+            this.namelbl.Text = "Name";
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.BackColor = System.Drawing.Color.DimGray;
+            this.Addbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addbtn.Location = new System.Drawing.Point(243, 356);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(108, 36);
+            this.Addbtn.TabIndex = 3;
+            this.Addbtn.Text = "Add";
+            this.Addbtn.UseVisualStyleBackColor = false;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
+            // Changebtn
+            // 
+            this.Changebtn.BackColor = System.Drawing.Color.DimGray;
+            this.Changebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Changebtn.Location = new System.Drawing.Point(245, 316);
+            this.Changebtn.Name = "Changebtn";
+            this.Changebtn.Size = new System.Drawing.Size(108, 36);
+            this.Changebtn.TabIndex = 3;
+            this.Changebtn.Text = "Change";
+            this.Changebtn.UseVisualStyleBackColor = false;
+            // 
+            // Surnametxtb
+            // 
+            this.Surnametxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Surnametxtb.Location = new System.Drawing.Point(112, 89);
+            this.Surnametxtb.Name = "Surnametxtb";
+            this.Surnametxtb.Size = new System.Drawing.Size(208, 26);
+            this.Surnametxtb.TabIndex = 2;
+            this.Surnametxtb.TextChanged += new System.EventHandler(this.Surnametxtb_TextChanged);
+            // 
+            // Nametxtb
+            // 
+            this.Nametxtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Nametxtb.Location = new System.Drawing.Point(111, 40);
+            this.Nametxtb.Name = "Nametxtb";
+            this.Nametxtb.Size = new System.Drawing.Size(208, 26);
+            this.Nametxtb.TabIndex = 2;
+            this.Nametxtb.TextChanged += new System.EventHandler(this.Nametxtb_TextChanged);
             // 
             // UserListBox
             // 
@@ -66,34 +242,14 @@
             this.UserListBox.Size = new System.Drawing.Size(255, 316);
             this.UserListBox.TabIndex = 1;
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(61, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(61, 212);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
-            // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.Color.DimGray;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox3.Location = new System.Drawing.Point(404, 356);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(255, 20);
+            this.textBox3.Size = new System.Drawing.Size(255, 26);
             this.textBox3.TabIndex = 2;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(61, 132);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 2;
             // 
             // Savebtn
             // 
@@ -116,28 +272,6 @@
             this.Loadbtn.TabIndex = 3;
             this.Loadbtn.Text = "Load";
             this.Loadbtn.UseVisualStyleBackColor = false;
-            // 
-            // Changebtn
-            // 
-            this.Changebtn.BackColor = System.Drawing.Color.Transparent;
-            this.Changebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Changebtn.Location = new System.Drawing.Point(243, 314);
-            this.Changebtn.Name = "Changebtn";
-            this.Changebtn.Size = new System.Drawing.Size(108, 36);
-            this.Changebtn.TabIndex = 3;
-            this.Changebtn.Text = "Change";
-            this.Changebtn.UseVisualStyleBackColor = false;
-            // 
-            // Addbtn
-            // 
-            this.Addbtn.BackColor = System.Drawing.Color.Transparent;
-            this.Addbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Addbtn.Location = new System.Drawing.Point(243, 356);
-            this.Addbtn.Name = "Addbtn";
-            this.Addbtn.Size = new System.Drawing.Size(108, 36);
-            this.Addbtn.TabIndex = 3;
-            this.Addbtn.Text = "Add";
-            this.Addbtn.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -165,14 +299,26 @@
         #endregion
 
         private System.Windows.Forms.GroupBox SurveygroupBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Surnametxtb;
+        private System.Windows.Forms.TextBox Nametxtb;
         private System.Windows.Forms.ListBox UserListBox;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button Addbtn;
         private System.Windows.Forms.Button Changebtn;
         private System.Windows.Forms.Button Savebtn;
         private System.Windows.Forms.Button Loadbtn;
+        private System.Windows.Forms.DateTimePicker BirthdatetimePicker;
+        private System.Windows.Forms.MaskedTextBox Phonemaskedtxtb;
+        private System.Windows.Forms.TextBox Emailtxtb;
+        private System.Windows.Forms.Label birthlbl;
+        private System.Windows.Forms.Label emaillbl;
+        private System.Windows.Forms.Label Phonelbl;
+        private System.Windows.Forms.Label surnamelbl;
+        private System.Windows.Forms.Label namelbl;
+        private System.Windows.Forms.Label emailchecklbl;
+        private System.Windows.Forms.Label phonechecklbl;
+        private System.Windows.Forms.Label surnamechecklbl;
+        private System.Windows.Forms.Label namechecklbl;
     }
 }
 
--- Survey/Form1.cs
+++ Survey/Form1.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace Survey
 {
@@ -31,10 +35,73 @@ namespace Survey
         {
 
         }
+        public bool Check { get; set; } = false;
+        private void Nametxtb_TextChanged(object sender, EventArgs e)
+        {
+            if (Nametxtb.Text.Length <= 0)
+            {
+                namechecklbl.Text = "Enter name!";
+                namechecklbl.ForeColor = Color.Red;
+            }
+            else
+            {
+                namechecklbl.Text = "";
+                Check = true;
+            }
+        }
+        private void Surnametxtb_TextChanged(object sender, EventArgs e)
+        {
+            if (Surnametxtb.Text.Length <= 0)
+            {
+                surnamechecklbl.Text = "Enter surname!";
+                surnamechecklbl.ForeColor = Color.Red;
+            }
+            else
+            {
+                surnamechecklbl.Text = "";
+                Check = true;
+            }
+        }
+        private void Emailtxtb_TextChanged(object sender, EventArgs e)
+        {
+            if (!Emailtxtb.Text.Contains("@gmail.com"))
+            {
+                emailchecklbl.Text = "Email is invalid!";
+                emailchecklbl.ForeColor = Color.Red;
+            }
+            else
+            {
+                emailchecklbl.Text = "";
+                Check = true;
+            }
+        }
 
-        private void SurveygroupBox1_Enter(object sender, EventArgs e)
+        private void Addbtn_Click(object sender, EventArgs e)
         {
 
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string BirthDate { get; set; }
+        public int MyProperty { get; set; }
+    }
+    public class FileHelper
+    {
+        public static void WriteJsonHuman(Person person)
+        {
+            var serializer = new JsonSerializer();
+            using (var sw = new StreamWriter($"{person.Name}.json"))
+            {
+                using (var jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                    serializer.Serialize(jw, person);
+                }
+            }
         }
     }
 }
